8c2d12c8b8a89be7611ac9ae7f8790d81cbaf6e7
Yenly <yencodes@gmail.com>
2020-08-29 00:37:11 -0700

    Transform wiki-links to use page title and add remark footnotes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 const rehypePrism = require('@mapbox/rehype-prism')
 const remarkImages = require('remark-images')
 const remarkExternalLinks = require('remark-external-links')
+const remarkFootnotes = require('remark-footnotes')
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkImages, remarkExternalLinks],
+    remarkPlugins: [
+      remarkImages,
+      remarkExternalLinks,
+      [remarkFootnotes, { inlineNotes: true }]
+    ],
     rehypePlugins: [rehypePrism]
   },
 })
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,18 @@ import '../styles/global.css'
 import '../styles/prism-theme-night-owl.css'
 import Layout from '../components/Layout'
 import siteData from '../siteconfig'
+import {MDXProvider} from '@mdx-js/react'
+
+const mdComponents = {
+    a: props => <a {...props}>{props.title || props.children}</a>
+}
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout siteData={siteData}>
-      <Component {...pageProps} />
-    </Layout>
+    <MDXProvider components={mdComponents}>
+      <Layout siteData={siteData}>
+        <Component {...pageProps} />
+      </Layout>
+    </MDXProvider>  
   )
 }
