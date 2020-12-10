a9a38a4a4734b03a8e8909ac7c7cd6d1078332a4
Yenly <yencodes@gmail.com>
2020-12-10 13:59:28 -0800

    Refactor Image component to remove layout fill option

--- components/Image.js
+++ components/Image.js
@@ -6,52 +6,35 @@ export default function Image(props) {
     alt,
     width = 600,
     height = 460,
-    layout = 'intrinsic',
-    objectFit = 'none',
-    objectPosition = '50% 50%' } = props
+    layout = 'intrinsic' } = props
   if(src.includes('?')) {
     const dimensions = src.split('?')
     const widthHeight = dimensions[1].split('x')
     width = widthHeight[0]
     height = widthHeight[1]
   }
-  switch(layout) {
-    case 'fixed':
-    case 'responsive':
-      return (
-        <>
-          <NextImage
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            layout={layout}
-          />
-        </>
+  if (layout === 'fixed' || layout === 'responsive') {
+    return (
+      <>
+        <NextImage
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          layout={layout}
+        />
+      </>
       )
-    case 'fill':
-      return (
-        <>
-          <NextImage
-            src={src}
-            alt={alt}
-            layout={layout}
-            objectFit={objectFit}
-            objectPosition={objectPosition}
-          />
-        </>
-      )
-    default:
-      return (
-        <>
-          <NextImage
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-          />
-        </>
-      )
+  } else {
+    return (
+      <>
+        <NextImage
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+        />
+      </>
+    )
   }
-
 }
