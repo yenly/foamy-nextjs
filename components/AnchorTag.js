73c51d6b357d22cb9433901945f6790e54d5eaf7
zomars <zomars@me.com>
2021-03-19 13:38:14 -0700

    Fixes footnote links

--- components/AnchorTag.js
+++ components/AnchorTag.js
@@ -2,10 +2,15 @@ import Link from 'next/link'
 
 const AnchorTag = (props) => {
   const { href, title, target, children } = props
-  if (target) {
+  const shouldBeSimpleLink = target || href.startsWith('#')
+  if (shouldBeSimpleLink) {
     return <a {...props}>{children}</a>
   }
-  return <Link href={href}><a>{title || children}</a></Link>
+  return (
+    <Link href={href}>
+      <a>{title || children}</a>
+    </Link>
+  )
 }
 
 export default AnchorTag
